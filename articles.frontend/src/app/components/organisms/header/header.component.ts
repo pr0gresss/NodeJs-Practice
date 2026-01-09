c0f6ca81f8e66d301bf5c7c2a93babfae06e7f14
pr0gresss <tsublenokd@mail.ru>
2026-01-09 18:35:44 +0200

    feat: Added nav link to users admin panel; Updated documentation;

--- articles.frontend/src/app/components/organisms/header/header.component.ts
+++ articles.frontend/src/app/components/organisms/header/header.component.ts
@@ -1,18 +1,33 @@
-import { Component, inject } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
-import { IconComponent } from '../../atoms/icon/icon.component';
-import { AuthService } from '../../../shared/services/auth.service';
-
+import {Component, inject, OnInit, signal} from "@angular/core";
+import {RouterLink, RouterLinkActive} from "@angular/router";
+import {IconComponent} from "../../atoms/icon/icon.component";
+import {AuthService} from "../../../shared/services/auth.service";
+import {RoleService} from "../../../shared/services/role.service";
 
 @Component({
-  standalone: true,
-  selector: 'app-header',
-  imports: [RouterLink, RouterLinkActive, IconComponent],
-  templateUrl: './header.component.html',
-  styleUrl: './header.component.scss',
+	standalone: true,
+	selector: "app-header",
+	imports: [RouterLink, RouterLinkActive, IconComponent],
+	templateUrl: "./header.component.html",
+	styleUrl: "./header.component.scss",
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+	private _roleService = inject(RoleService);
 	public authService = inject(AuthService);
+	public isAdmin = signal(false);
+
+	ngOnInit(): void {
+		this._roleService.getAll().subscribe({
+			next: roles => {
+				this.authService.getMe().subscribe({next:(user) => {
+					this.isAdmin.set(
+					roles.filter(role => role.name === "Admin")[0]!.id ==
+						user.roleId!
+				);
+				}})
+			},
+		});
+	}
 
 	public signOut() {
 		this.authService.signOut();
